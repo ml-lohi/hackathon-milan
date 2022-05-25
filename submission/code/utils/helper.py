8df19479e796b66356c1e9a77b9dbb0e89653888
Kostiantyn Lavronenko <lavronenkostya@gmail.com>
2022-05-25 14:31:04 +0200

    update helper & smoothing

--- submission/code/utils/helper.py
+++ submission/code/utils/helper.py
@@ -9,17 +9,33 @@ def read_data(file):
     data = np.loadtxt(file, dtype=np.complex128)
     return data.reshape(data.shape[0], 5, 3, 64, 64)
 
-
-def blur(data, length, N_radar):
+def blur(data):
+    """
+    data shape (n_times, n_radars, (image))
+    """
     data_blurred = data.copy()
-    for i in range(length):
-        for j in range(N_radar):
+    for i in range(data.shape[0]):
+        for j in range(data.shape[1]):
             data_blurred[i,j] = cv2.GaussianBlur(data_blurred[i,j,:,:], ksize=(5,5), sigmaX=1)
     return data_blurred
 
-def to_real(data, length, N_radar):
+def morphology(data):
+    """
+    data shape (n_times, n_radars, (image))
+    """
+    data_blurred = data.copy()
+    kernel = np.ones((5,5),np.uint8)
+    for i in range(data.shape[0]):
+        for j in range(data.shape[1]):
+            data_blurred[i,j] =  cv2.morphologyEx(data_blurred[i,j,:,:], cv2.MORPH_CLOSE, kernel)
+    return data_blurred
+
+def to_real(data):
+    """
+    data shape (n_times, n_radars, (image))
+    """
     data_real = np.asarray(data)
-    for i in range(length):
-        for j in range(N_radar):
+    for i in range(data.shape[0]):
+        for j in range(data.shape[1]):
             data_real[i,j] = np.abs(data)[i,j,:,:]
     return data_real.astype(float)
