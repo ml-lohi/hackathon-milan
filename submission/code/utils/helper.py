2096229ea17a8094c45bc7d2b1aa98a13dcd44c6
bamboo_user <sviatoslavs999@gmail.com>
2022-05-26 16:41:28 +0200

    multiple commits

--- submission/code/utils/helper.py
+++ submission/code/utils/helper.py
@@ -5,9 +5,10 @@ from scipy.fftpack import fft
 import cv2
 
 
-def read_data(file, n_frames:int = 5):
+def read_data(file, n_frames: int = 5):
     data = np.loadtxt(file, dtype=np.complex128)
     return data.reshape(-1, n_frames, 3, 64, 64)
+
 
 def blur(data):
     """
@@ -16,25 +17,33 @@ def blur(data):
     data_blurred = data.copy()
     for i in range(data.shape[0]):
         for j in range(data.shape[1]):
-            data_blurred[i,j] = cv2.GaussianBlur(data_blurred[i,j,:,:], ksize=(5,5), sigmaX=1)
+            data_blurred[i, j] = cv2.GaussianBlur(
+                data_blurred[i, j, :, :], ksize=(5, 5), sigmaX=1
+            )
     return data_blurred
+
 
 def morphology(data):
     """
     data shape (n_times, n_radars, (image))
     """
     data_blurred = data.copy()
-    kernel = np.ones((5,5),np.uint8)
+    kernel = np.ones((5, 5), np.uint8)
     for i in range(data.shape[0]):
         for j in range(data.shape[1]):
-            data_blurred[i,j] =  cv2.morphologyEx(data_blurred[i,j,:,:], cv2.MORPH_OPEN, kernel)
+            data_blurred[i, j] = cv2.morphologyEx(
+                data_blurred[i, j, :, :], cv2.MORPH_OPEN, kernel
+            )
     return data_blurred
+
 
 def gauss(x, sigma):
     return np.exp(-(x**2) / (2 * sigma**2)) / (np.sqrt(2 * np.pi) * sigma)
 
+
 def gaussdxdx(x, sigma):
     return ((sigma**2 - x**2) / sigma**4) * gauss(x, sigma)
+
 
 def filter_gaussdxdx(image, kernel_factor, sigma):
     faktor = kernel_factor * sigma * 2 + 1
